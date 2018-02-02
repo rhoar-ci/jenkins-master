48dba6bf2c4a7651a19b58be1c4586241175e3d9
Ladislav Thon <lthon@redhat.com>
2018-02-02 11:37:46 +0100

    configure GitHub Pull Request Builder plugin explicitly
    This includes setting the "request for testing" phrase, which
    isn't initialized in the plugin, which causes a NPE.

--- scripts/init.groovy.d/21-ghprb.groovy
+++ scripts/init.groovy.d/21-ghprb.groovy
@@ -13,6 +13,16 @@ def data = [
 
 def descriptor = Jenkins.instance.getDescriptorByType(GhprbTrigger.DescriptorImpl.class)
 
+// some of these variables are not initialized to a value
+// some other are, but the source code says it's only meant for testing and shouldn't be considered a default value
+// so here we set all the relevant values explicitly
+descriptor.manageWebhooks = true
+descriptor.whitelistPhrase = '.*add\\W+to\\W+whitelist.*'
+descriptor.okToTestPhrase = '.*ok\\W+to\\W+test.*'
+descriptor.retestPhrase = '.*test\\W+this\\W+please.*'
+descriptor.skipBuildPhrase = '.*\\[skip\\W+ci\\].*'
+descriptor.requestForTestingPhrase = 'Can one of the admins verify this patch?'
+descriptor.cron = 'H/5 * * * *'
 descriptor.githubAuth = [
         new GhprbGitHubAuth(data.githubUrl, data.jenkinsUrl, data.credentialsId, data.description, data.id, data.sharedSecret)
 ]
